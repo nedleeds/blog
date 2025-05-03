cb1e3bb3541b86f272484ccf7b819775e3f713df
nedleeds <lllee.dh@gmail.com>
2025-05-03 20:51:04 +0900

    Remove submodule and convert hextra to regular folder

---
+++ themes/hextra/static/js/robot.js
@@ -0,0 +1,23 @@
+import * as THREE from "https://cdn.skypack.dev/three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(300, 300);
+document.getElementById("robot-canvas").appendChild(renderer.domElement);
+
+// 큐브 예시
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 2;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
